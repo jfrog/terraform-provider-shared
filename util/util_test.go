75e38df8f1e30d956ac92d524d946a25ea638aad
Alex Hung <alexh@jfrog.com>
2023-03-17 11:32:58 -0700

    Add nil check for CastToStringArr

---
+++ util/util_test.go
@@ -0,0 +1,27 @@
+package util
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestCastToStringArr(t *testing.T) {
+	testSlice := []interface{}{"a", "b", "c"}
+
+	result := CastToStringArr(testSlice)
+
+	expected := []string{"a", "b", "c"}
+	if reflect.DeepEqual(expected, result) == false {
+		t.Errorf("Incorrect slice. Expected %v: got: %v", expected, result)
+	}
+}
+
+func TestCastToStringArr_exclude_nils(t *testing.T) {
+	testSlice := []interface{}{"a", "b", nil}
+
+	result := CastToStringArr(testSlice)
+
+	if len(result) != 2 {
+		t.Errorf("Incorrect length. Expected: 2, got: %d", len(result))
+	}
+}
