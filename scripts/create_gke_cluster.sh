7f9a6928f9dd267310e15a942e763e11b6e4384e
Daniel Miakotkin <danielmi@jfrog.com>
2023-03-23 13:38:23 -0700

    testing scripts

---
+++ scripts/create_gke_cluster.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+set -e
+
+echo "--------------------------------------------------------------------"
+echo "###                 Create GKE k8s cluster                        ###"
+echo "--------------------------------------------------------------------"
+
+if [ -n "${SERVICE_ACCOUNT_JSON}" ]; then
+  echo "Authenticating with service account JSON file"
+  gcloud auth activate-service-account --key-file="${SERVICE_ACCOUNT_JSON}"
+fi
+
+gcloud container clusters create "${GKE_CLUSTER}" --zone "${GKE_ZONE}" \
+      --node-locations "${GKE_ZONE}" --num-nodes "${NUM_NODES:-5}" --enable-autoscaling \
+      --machine-type "${MACHINE_TYPE}" \
+      --disk-size 50Gi \
+      --min-nodes 1 --max-nodes 5 --project "${GKE_PROJECT}" \
+      --enable-master-authorized-networks \
+      --master-authorized-networks 52.215.237.185/32,52.9.243.19/32,"${WHITELIST_CIDR}"
+      # add your NAT CIDR to whitelist local or CI/CD NAT IP. Set WHITELIST_CIDR in CI/CD to add CIDR to the list automatically.
+gcloud container clusters get-credentials "${GKE_CLUSTER}" --zone "${GKE_ZONE}" --project "${GKE_PROJECT}"
