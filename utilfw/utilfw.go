e12265d07dda40c23b9a6d3c3b5a5402b6d00291
Daniel Miakotkin <danielmi@jfrog.com>
2023-05-02 16:40:48 -0700

    Use terraform-plugin-framework. Add utilfw to use in a new resources migrated to the framework.

---
+++ utilfw/utilfw.go
@@ -0,0 +1,36 @@
+package utilfw
+
+import (
+	"fmt"
+
+	"github.com/hashicorp/terraform-plugin-framework/attr"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+)
+
+// ValueToSetType ensures we have a types.Set literal
+func ValueToSetType(v attr.Value) types.Set {
+	if vb, ok := v.(types.Set); ok {
+		return vb
+	} else if vb, ok := v.(*types.Set); ok {
+		return *vb
+	} else {
+		panic(fmt.Sprintf("cannot pass type %T to conv.ValueToSetType", v))
+	}
+}
+
+// AttributeValueToString will attempt to execute the appropriate AttributeStringerFunc from the ones registered.
+func AttributeValueToString(v attr.Value) string {
+	if s, ok := v.(types.String); ok {
+		return s.ValueString()
+	}
+	return v.String()
+}
+
+func StringSetToStrings(v attr.Value) []string {
+	vt := ValueToSetType(v)
+	out := make([]string, len(vt.Elements()))
+	for i, ve := range vt.Elements() {
+		out[i] = AttributeValueToString(ve)
+	}
+	return out
+}
